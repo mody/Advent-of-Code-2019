141ecaca043f14b5d38190f4253f9fe0d9921de9
Patrik Modesto <patrik.modesto@gmail.com>
2021-06-09 12:49:02 +0200

    level1, part2

--- src/level1.cc
+++ src/level1.cc
@@ -2,16 +2,26 @@
 #include <string>
 #include <vector>
 
+int calcFuel(unsigned mass) {
+    return mass / 3 - 2;
+}
+
+
 int main(int argc, char* argv[]) {
-    unsigned sum = 0;
+    unsigned sum1 = 0, sum2 = 0;
 
     for (;;) {
-        unsigned num = 0;
-        std::cin >> num;
+        unsigned mass = 0;
+        std::cin >> mass;
         if (std::cin.eof()) { break; }
-        sum += num / 3 - 2;
+        int fuel = calcFuel(mass);
+        sum1 += fuel;
+        while(fuel > 0) {
+            sum2 += fuel;
+            fuel = calcFuel(fuel);
+        }
     }
 
-    std::cout << "1: " << sum << std::endl;
+    std::cout << "1: " << sum1 << std::endl << "2: " << sum2 << std::endl;
     return 0;
 }
