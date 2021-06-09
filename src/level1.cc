237d4dbd0bfd0e53273ae27892e104df455dd628
Patrik Modesto <patrik.modesto@gmail.com>
2021-06-09 12:42:43 +0200

    level1, part1

--- src/level1.cc
+++ src/level1.cc
@@ -3,5 +3,15 @@
 #include <vector>
 
 int main(int argc, char* argv[]) {
+    unsigned sum = 0;
+
+    for (;;) {
+        unsigned num = 0;
+        std::cin >> num;
+        if (std::cin.eof()) { break; }
+        sum += num / 3 - 2;
+    }
+
+    std::cout << "1: " << sum << std::endl;
     return 0;
 }
