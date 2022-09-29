ff11e42347141f45bd38039405496210f6b4a2e1
Patrik Modesto <patrik.modesto@gmail.com>
2022-09-29 20:28:50 +0200

    level18, part2

--- src/level18.cc
+++ src/level18.cc
@@ -1,3 +1,4 @@
+#include <array>
 #include <boost/config.hpp>
 #include <boost/container_hash/hash.hpp>
 #include <boost/graph/adjacency_list.hpp>
@@ -15,10 +16,33 @@
 #include <string>
 #include <unordered_map>
 
+// #define DUMP
+
+using CoordType = int;
+
+struct Player {
+    CoordType x {}, y {};
+
+    bool operator== (const Player& oth) const noexcept {
+        return std::pair(x, y) == std::pair(oth.x, oth.y);
+    }
+
+    bool operator< (const Player& oth) const noexcept {
+        return std::pair(x, y) < std::pair(oth.x, oth.y);
+    }
+
+    friend size_t hash_value(Player const& p) noexcept {
+        size_t seed = 0;
+        boost::hash_combine(seed, p.x);
+        boost::hash_combine(seed, p.y);
+        return seed;
+    }
+};
+
 struct Mapa
 {
-    using CoordType = int;
     using KeyDistanceMap = std::map<char, unsigned>;
+    using PlayerKeyDistanceMap = std::map<unsigned, KeyDistanceMap>;
 
     void append(std::string const& line)
     {
@@ -27,9 +51,11 @@ struct Mapa
         for (auto c : line) {
             data.push_back(c);
         }
-        auto idx = data.find('@');
-        if (idx != std::string::npos) {
-            std::tie(me_x, me_y) = index_to_xy(idx);
+        players.clear();
+        unsigned pos = 0;
+        for (auto idx = data.find('@'); idx != std::string::npos; idx = data.find('@', idx + 1)) {
+            auto [x, y] = index_to_xy(idx);
+            players.insert({pos++, {x, y}});
         }
     }
 
@@ -44,6 +70,10 @@ struct Mapa
             std::cout << line << "\n";
             line.clear();
         }
+        std::cout << std::hex << "keys: 0x" << owned_keys << std::dec << "\n";
+        for (auto const& [ppos, player] : players) {
+            std::cout << "P" << ppos << ": {" << player.x << ", " << player.y << "}\n";
+        }
     }
 
     size_t size() const
@@ -51,22 +81,12 @@ struct Mapa
         return data.size();
     }
 
-    int keys() const {
-        int count = 0;
-        for (auto const& c : data) {
-            if (c >= 'a' && c <= 'z') {
-                ++count;
-            }
-        }
-        return count;
+    std::string get_data() const noexcept {
+        return data;
     }
 
-    KeyDistanceMap calculate_distances() const
+    PlayerKeyDistanceMap calculate_distances() const
     {
-        CoordType origin = data.find('@');
-        assert(origin != data.npos);
-        assert(origin == xy_to_index(me_x, me_y));
-
         using EdgeWeightProp = boost::property<boost::edge_weight_t, unsigned>;
         using Graph = boost::adjacency_list<
             boost::vecS, boost::vecS, boost::bidirectionalS, boost::property<boost::vertex_distance_t, unsigned>,
@@ -75,7 +95,7 @@ struct Mapa
 
         Graph g(data.size());
 
-        auto add_edge = [&](Vertex from, Mapa::CoordType x, Mapa::CoordType y) {
+        auto add_edge = [&](Vertex from, CoordType x, CoordType y) {
             const auto c = get(x, y);
             if (c == '#' || (c >= 'A' && c <= 'Z')) {
                 return;
@@ -98,25 +118,36 @@ struct Mapa
             if (has(x, y + 1)) { add_edge(from, x, y + 1); }
         }
 
-        boost::dijkstra_shortest_paths(
-            g, boost::vertex(origin, g), boost::distance_map(boost::get(boost::vertex_distance, g)));
+        PlayerKeyDistanceMap pkd;
 
-        auto const& dist = boost::get(boost::vertex_distance, g);
-        KeyDistanceMap kd;
-        for (size_t idx = 0; idx < data.size(); ++idx) {
-            auto const& c = data.at(idx);
-            if (c >= 'a' && c <= 'z') {
-                auto d = dist[boost::vertex(idx, g)];
-                if (d < std::numeric_limits<int>::max()) {
-                    kd[c] = d;
+        for (auto const& [ppos, p] : players) {
+            CoordType origin = xy_to_index(p.x, p.y);
+            assert(origin != data.npos);
+
+            auto& kd = pkd[ppos];
+
+            boost::dijkstra_shortest_paths(
+                g, boost::vertex(origin, g), boost::distance_map(boost::get(boost::vertex_distance, g)));
+
+            auto const& dist = boost::get(boost::vertex_distance, g);
+            for (size_t idx = 0; idx < data.size(); ++idx) {
+                auto const& c = data.at(idx);
+                if (c >= 'a' && c <= 'z') {
+                    auto d = dist[boost::vertex(idx, g)];
+                    if (d < std::numeric_limits<int>::max()) {
+#ifdef DUMP
+                        std::cout << "P: " << ppos << ", dist to " << c << " is " << d << std::endl;
+#endif
+                        kd[c] = d;
+                    }
                 }
             }
         }
 
-        return kd;
+        return pkd;
     }
 
-    bool has(CoordType x, CoordType y) const
+    bool has(CoordType x, CoordType y) const noexcept
     {
         return (x >= 0 && y >= 0 && x < max_x && y < max_y);
     }
@@ -127,39 +158,45 @@ struct Mapa
         return data.at(y * max_x + x);
     }
 
-    CoordType xy_to_index(CoordType x, CoordType y) const
+    CoordType xy_to_index(CoordType x, CoordType y) const noexcept
     {
         return (y * max_x + x);
     }
 
-    std::pair<CoordType, CoordType> index_to_xy(CoordType index) const
+    std::pair<CoordType, CoordType> index_to_xy(CoordType index) const noexcept
     {
         CoordType x = index % max_x;
         CoordType y = index / max_x;
         return {x, y};
     }
 
-    void move_me(char k)
-    {
-        CoordType me = data.find('@');
-        assert(me != data.npos);
-
-        CoordType key = data.find(k);
-        assert(key != data.npos);
-
-        open_door(k);
-
-        data[me] = '.';
-        data[key] = '@';
-
-        std::tie(me_x, me_y) = index_to_xy(key);
+    Player get_player(unsigned ppos) const {
+        return players.at(ppos);
     }
 
-    friend size_t hash_value(Mapa const& mapa) {
+    std::pair<CoordType, CoordType> get_max() const noexcept { return {max_x, max_y}; }
+
+    void move_player(unsigned ppos, char key)
+    {
+        CoordType keyPos = data.find(key);
+        assert(keyPos != data.npos);
+
+        open_door(key);
+
+        Player& player = players.at(ppos);
+
+        data[xy_to_index(player.x, player.y)] = '.';
+        data[keyPos] = '@';
+
+        std::tie(player.x, player.y) = index_to_xy(keyPos);
+    }
+
+    friend size_t hash_value(Mapa const& mapa) noexcept {
         size_t seed = 0;
-        // boost::hash_combine(seed, mapa.data);
-        boost::hash_combine(seed, mapa.me_x);
-        boost::hash_combine(seed, mapa.me_y);
+        for (auto const& [ppos, p] : mapa.players) {
+            boost::hash_combine(seed, ppos);
+            boost::hash_combine(seed, p);
+        }
         boost::hash_combine(seed, mapa.owned_keys);
         return seed;
     }
@@ -168,8 +205,7 @@ struct Mapa
     {
         assert(max_x == o.max_x);
         assert(max_y == o.max_y);
-        // return data == o.data && owned_keys == o.owned_keys;
-        return me_x == o.me_x && me_y == o.me_y && owned_keys == o.owned_keys;
+        return owned_keys == o.owned_keys && players == o.players;
     }
 
 protected:
@@ -187,12 +223,13 @@ protected:
         if (door == data.npos) {
             return;
         }
+        assert(door < data.size());
         data[door] = '.';
     }
 
-    CoordType me_x = 0, me_y = 0;
+    std::map<unsigned, Player> players;
     CoordType max_x = 0, max_y = 0;
-    std::string data;
+    std::string data {};
     unsigned owned_keys = 0;
 };
 
@@ -242,32 +279,49 @@ int part1(Mapa mapa)
 
         const Vertex from = states.at(m);
 
-        // std::cout << "\n";
-        // m.dump();
+#ifdef DUMP
+        std::cout << "\n";
+        m.dump();
+#endif
 
         auto next_moves = m.calculate_distances();
-        // std::cout << "next_moves: " << next_moves.size() << std::endl;
-        if (next_moves.empty()) {
-            // end
-            // std::cout << "FINISHED" << std::endl;
-            boost::add_edge(from, dst, EdgeWeightProp{0}, g);
-            continue;
+
+        unsigned empty_moves = 0;
+        for (auto const& [ppos, moves] : next_moves) {
+#ifdef DUMP
+            std::cout << "moves: " << moves.size() << std::endl;
+#endif
+            if (moves.empty()) {
+                // end
+                ++empty_moves;
+                continue;
+            }
+
+            for (auto const& [key, dist] : moves) {
+                Mapa m2 = m;
+                m2.move_player(ppos, key);
+                auto it = states.find(m2);
+                if (it == states.end()) {
+                    Vertex to = boost::add_vertex(g);
+                    auto const& [_, in] = boost::add_edge(from, to, EdgeWeightProp {dist}, g);
+                    assert(in);
+                    states.insert({m2, to});
+                    states_queue.push_back(std::move(m2));
+                } else {
+#ifdef DUMP
+                    std::cout << "state already seen; new dist " << dist << "\n";
+#endif
+                    auto const& [_, in] = boost::add_edge(from, it->second, EdgeWeightProp {dist}, g);
+                    assert(in);
+                }
+            }
         }
 
-        for (auto const& [key, dist] : next_moves) {
-            Mapa m2 = m;
-            m2.move_me(key);
-            auto it = states.find(m2);
-            if (it == states.end()) {
-                Vertex to = boost::add_vertex(g);
-                auto const& [_, in] = boost::add_edge(from, to, EdgeWeightProp{dist}, g);
-                assert(in);
-                states.insert({m2, to});
-                states_queue.push_back(std::move(m2));
-            } else {
-                auto const& [_, in] = boost::add_edge(from, it->second, EdgeWeightProp{dist}, g);
-                assert(in);
-            }
+        if (empty_moves == next_moves.size()) {
+#ifdef DUMP
+            std::cout << "FINISHED" << std::endl;
+#endif
+            boost::add_edge(from, dst, EdgeWeightProp {0}, g);
         }
     }
 
@@ -279,6 +333,40 @@ int part1(Mapa mapa)
 
     return dist_map[dst];
 }
+
+unsigned part2(Mapa const& m1)
+{
+    std::string data = m1.get_data();
+    auto const& [me_x, me_y] = m1.get_player(0);
+    auto const& [max_x, max_y] = m1.get_max();
+
+    assert(me_x == (max_x / 2));
+    assert(me_y == (max_y / 2));
+
+    assert(me_x > 1 && me_x + 1 < max_x);
+    assert(me_y > 1 && me_y + 1 < max_y);
+
+    data[m1.xy_to_index(me_x, me_y)] = '#';
+    data[m1.xy_to_index(me_x - 1, me_y)] = '#';
+    data[m1.xy_to_index(me_x + 1, me_y)] = '#';
+    data[m1.xy_to_index(me_x, me_y - 1)] = '#';
+    data[m1.xy_to_index(me_x, me_y + 1)] = '#';
+
+    data[m1.xy_to_index(me_x - 1, me_y - 1)] = '@';
+    data[m1.xy_to_index(me_x + 1, me_y + 1)] = '@';
+    data[m1.xy_to_index(me_x + 1, me_y - 1)] = '@';
+    data[m1.xy_to_index(me_x - 1, me_y + 1)] = '@';
+
+    Mapa m2;
+    for (unsigned y = 0; y < max_y; ++y) {
+        m2.append(data.substr(y * max_x, max_x));
+    }
+    // m1.dump();
+    // m2.dump();
+
+    return part1(m2);
+}
+
 
 int main()
 {
@@ -294,6 +382,9 @@ int main()
 
     int r1 = part1(mapa);
     std::cout << "1: " << r1 << "\n";
+
+    int r2 = part2(mapa);
+    std::cout << "2: " << r2 << "\n";
 
     return 0;
 }
